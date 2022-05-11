32fe67dd2a74860c013997cb3114789f5e6b4d3e
YashMaurya9274 <yash.maurya537468@gmail.com>
2022-05-12 02:10:52 +0530

    twitter2

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,8 +6,8 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID,
